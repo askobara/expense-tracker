b74ca8387b9cee09d15a61b819659d934604b689
Arthur Skobara <skobara.arthur@gmail.com>
2023-11-21 14:55:17 +0700

    use basic prompts for user inputs

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,21 @@ use std::collections::HashMap;
 use std::env;
 use std::str::FromStr;
 
-trait CustomProperties {
-    fn category_db(&self) -> Option<Vec<notion::ids::PageId>>;
-}
-
-impl CustomProperties for notion::models::Properties
-{
-    fn category_db(&self) -> Option<Vec<notion::ids::PageId>> {
-        self.properties.values().find_map(|p| match p {
-            notion::models::properties::PropertyValue::Relation { relation, .. } => {
-                Some(relation.as_ref().unwrap_or(&vec![]).iter().map(|t| t.id.clone()).collect())
-            }
-            _ => None,
-        })
+fn select_page(pages: Vec<notion::models::Page>) -> Result<notion::ids::PageId> {
+    struct Page {
+        page: notion::models::Page,
     }
+
+    impl std::fmt::Display for Page {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "{}", self.page.title().unwrap_or("Untitled".to_string()))
+        }
+    }
+
+    let options = pages.into_iter().map(|page| Page { page }).collect();
+    let result = inquire::Select::new("Category:", options).prompt()?;
+
+    Ok(result.page.id.clone())
 }
 
 #[tokio::main]
@@ -28,24 +29,16 @@ async fn main() -> Result<()> {
     let db_id = notion::ids::DatabaseId::from_str(&env::var("NOTION_DATABASE_ID")?)?;
     let db: notion::models::Database = notion_api.get_database(db_id).await?;
 
-    // let result = notion_api.query_database(
-    //     &db.id,
-    //     notion::models::search::DatabaseQuery::default()
-    // ).await?;
-    //
-    // dbg!(result);
-    //
-    // return Ok(());
-
     let mut properties: HashMap<String, notion::models::properties::PropertyValue> = HashMap::new();
 
     match db.properties.get("Name") {
         Some(notion::models::properties::PropertyConfiguration::Title { id }) => {
+            let name = inquire::Text::new("Name:").prompt()?;
 
             let title = vec![
                 notion::models::text::RichText::Text {
-                    rich_text: notion::models::text::RichTextCommon { plain_text: "test".to_string(), href: None, annotations: None },
-                    text: notion::models::text::Text { content: "test".to_string(), link: None }
+                    rich_text: notion::models::text::RichTextCommon { plain_text: name.clone(), href: None, annotations: None },
+                    text: notion::models::text::Text { content: name, link: None }
                 }
             ];
 
@@ -59,9 +52,11 @@ async fn main() -> Result<()> {
 
     match db.properties.get("Amount") {
         Some(notion::models::properties::PropertyConfiguration::Number { id, .. }) => {
+            let amount = inquire::Text::new("Amount:").prompt()?;
+
             properties.insert(
                 "Amount".to_string(),
-                notion::models::properties::PropertyValue::Number { id: id.clone(), number: Some(serde_json::Number::from(100) ) }
+                notion::models::properties::PropertyValue::Number { id: id.clone(), number: serde_json::Number::from_f64(amount.parse::<f64>()?) }
             );
         }
         _ => {}
@@ -71,13 +66,20 @@ async fn main() -> Result<()> {
         Some(notion::models::properties::PropertyConfiguration::Date { id }) => {
 
             let now = notion::chrono::offset::Local::now();
+            let date = inquire::DateSelect::new("Date:")
+                .with_default(now.date_naive())
+                .with_min_date(now.checked_sub_days(notion::chrono::Days::new(7)).unwrap().date_naive())
+                .with_max_date(now.date_naive())
+                .with_week_start(notion::chrono::Weekday::Mon)
+                // .with_help_message("Possible flights will be displayed according to the selected date")
+                .prompt()?;
 
             properties.insert(
                 "Date".to_string(),
                 notion::models::properties::PropertyValue::Date {
                     id: id.clone(),
                     date: Some(notion::models::properties::DateValue {
-                        start: notion::models::properties::DateOrDateTime::Date(now.date_naive()),
+                        start: notion::models::properties::DateOrDateTime::Date(date),
                         end: None,
                         time_zone: None
                     })
@@ -94,9 +96,8 @@ async fn main() -> Result<()> {
                 notion::models::search::DatabaseQuery::default()
             ).await?;
 
-            let f = result.results.first().unwrap(); // TODO: add skim
-
-            let r = vec![notion::models::properties::RelationValue { id: f.id.clone() }];
+            let page_id = select_page(result.results)?;
+            let r = vec![notion::models::properties::RelationValue { id: page_id }];
 
             properties.insert(
                 "Category".to_string(),
